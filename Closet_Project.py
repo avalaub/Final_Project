5387c50cdf81bd9f7915b356d532741d6a51bd7a
ava2721@gmail.com <ava2721@gmail.com>
2023-04-11 11:02:04 -0400

    added main function that opens json file

--- Closet_Project.py
+++ Closet_Project.py
@@ -8,3 +8,8 @@ import json
 if __name__ == "__main__":
     args = parse_args(sys.argv[1:])
     main(args.filepath, args.weather, args.tops, args.pants, args.shoes, args.accessories)
+
+
+def main(filepath):
+    with open(filepath, "r", encoding="utf-8") as f:
+        closet = json.load(f)
